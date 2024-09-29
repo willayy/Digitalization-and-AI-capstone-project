2371eb26e310c5aa1d18d61b39354ca403ce5ae4
Mandus <mandus.hogberg@gmail.com>
2024-09-29 22:30:25 +0200

    course code added to css

--- Innovation website/innovation-website/src/app/about/page.tsx
+++ Innovation website/innovation-website/src/app/about/page.tsx
@@ -3,49 +3,53 @@ import styles from'./page.module.css';
 export default function About() {
   return (
     <div className={styles.aboutPage}>
-        <div>
-          <h1>About Us</h1>
-          <p>This is the about page.</p>
+      <div>
+        <h1>Who are we?</h1>
+      </div>
+
+      <div>
+        <h2 className={styles.developersTitle}>Developers</h2>
+      </div>
+
+      <div className={styles.peopleSection}>
+        <div className={styles.person}>
+          <h3>Jonatan Markusson</h3>
+          <p>Contact info: jomarkusson@gmail.com</p>
         </div>
 
-        <div>
-           <h2 className={styles.developersTitle}>Utvecklare</h2>
+        <div className={styles.person}>
+          <h3>Max Dreifeldt</h3>
+          <p>Contact info: maxdreifeldt02@gmail.com</p>
         </div>
 
-        <div className={styles.peopleSection}>
-
-            <div className={styles.person}>
-                <h3>Jonatan Markusson</h3>
-                <p>Kontaktinfo: jomarkusson@gmail.com</p>
-            </div>
-
-            <div className={styles.person}>
-                <h3>Max Dreifeldt</h3>
-                <p>Kontaktinfo: maxdreifeldt02@gmail.com</p>
-            </div>
-
-            <div className={styles.person}>
-                <h3>William Norland</h3>
-                <p>Kontaktinfo: williamnorland@gmail.com</p>
-            </div>
-
-            <div className={styles.person}>
-                <h3>Mandus Högberg</h3>
-                <p>Kontaktinfo: mandus.hogberg@gmail.com</p>
-            </div>
-            <div className={styles.person}>
-                <h3>Max Levin</h3>
-                <p>Kontaktinfo: ...@gmail.com</p>
-            </div>
-            <div className={styles.person}>
-                <h3>Alexander Muhr</h3>
-                <p>Kontaktinfo: alle.muhr@gmail.com</p>
-            </div>
-        <div className={styles.imagediv}>
-            <img src="chalmers_logo.png" alt="Chalmers Logo" />
+        <div className={styles.person}>
+          <h3>William Norland</h3>
+          <p>Contact info: williamnorland@gmail.com</p>
         </div>
 
+        <div className={styles.person}>
+          <h3>Mandus Högberg</h3>
+          <p>Contact info: mandus.hogberg@gmail.com</p>
         </div>
+
+        <div className={styles.person}>
+          <h3>Max Levin</h3>
+          <p>Contact info: ...@gmail.com</p>
+        </div>
+
+        <div className={styles.person}>
+          <h3>Alexander Muhr</h3>
+          <p>Contact info: alle.muhr@gmail.com</p>
+        </div>
+      </div>
+
+      <div>
+        <h2>TEK830</h2>
+      </div>
+
+      <div className={styles.imagediv}>
+        <img src="chalmers_logo.png" alt="Chalmers Logo" />
+      </div>
     </div>
   );
 }
