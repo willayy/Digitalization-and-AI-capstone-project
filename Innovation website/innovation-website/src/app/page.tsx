4a8c886dbbe5cf155d9a0d4495c09403dfec4214
Mandus <mandus.hogberg@gmail.com>
2024-09-30 00:19:30 +0200

    demo page cleanup

--- Innovation website/innovation-website/src/app/page.tsx
+++ Innovation website/innovation-website/src/app/page.tsx
@@ -8,16 +8,43 @@ export default function Home() {
   const specificH2Ref = useRef<HTMLHeadingElement | null>(null);
   return (
     <div className={styles.homePage}>
+
+        <div className={styles.video}>
           <h1>Welcome to the future of marketing!</h1>
           <VideoPlayer videoSrc="spoderman.mp4" h2Ref={specificH2Ref} />
+        </div>
 
-          <h2 ref={specificH2Ref}>The design behind</h2>
-
+        <div>
+          <h2 className={styles.designTitle} ref={specificH2Ref}>The design behind</h2>
           <p>
             Supercool text about design thinking principles and how good we are at making stuff.
           </p>
-          <p>hihi</p>
+          <br />
+          <div >
+            <h2> Design 1 </h2>
+            <br />
+            <p>
+            thing about design 1
+            </p>
+            <br />
+
+            <h2> Design 2 </h2>
+            <br />
+            <p>
+            thing about design 2
+            </p>
+            <br />
+
+            <h2> Design 3 </h2>
+            <br />
+            <p>
+            thing about design 3
+            </p>
+            <br />
+
+          </div>
         </div>
-      );
+    </div>
+    );
 
 }
