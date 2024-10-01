f823b5bec32a487e3ae53f1e48238416ade34828
Snapz <16058149+themaxlevin@users.noreply.github.com>
2024-10-01 16:20:32 +0200

    Referenser

--- Innovation website/innovation-website/src/app/references/page.tsx
+++ Innovation website/innovation-website/src/app/references/page.tsx
@@ -1,9 +1,14 @@
+import styles from './page.module.css';
+
 export default function Discussion() {
   return (
     <div>
-        <div>
-          <h1>References</h1>
-          <p>This is the references page.</p>
+        <div className={styles.body}>
+          <h1 className={styles.text}>References</h1>
+          <ol className={styles.list}>
+            <li className={styles.text}>Hage, S., Francl, T., Bardou, E., & Söderqvist, J. (2024). <i>IKEA X Chalmers Kickoff</i> [PowerPoint Slides]. Digitalization and AI in practice, Chalmers Tekniska Högskola. September 5.</li>
+            <li className={styles.text}>Teigland, R., & Heathcote-Fumador, I. (2024). <i>Design Thinking</i> [PowerPoint Slides]. Digitalization and AI in practice, Chalmers Tekniska Högskola. September 10.</li>
+          </ol>
         </div>
     </div>
   );
