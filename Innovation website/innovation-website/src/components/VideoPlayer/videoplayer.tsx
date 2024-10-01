ddf30852cb1562ae60d1f46e9987b5ec7f86bdc3
Mandus <mandus.hogberg@gmail.com>
2024-10-01 16:12:58 +0200

    quick fix

--- Innovation website/innovation-website/src/components/VideoPlayer/videoplayer.tsx
+++ Innovation website/innovation-website/src/components/VideoPlayer/videoplayer.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useRef } from 'react';
+//import React, { useRef } from 'react';
 
 interface VideoPlayerProps {
   videoSrc: string;  //String name of video to be played
