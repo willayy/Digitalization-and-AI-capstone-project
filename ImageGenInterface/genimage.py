090285c9c45ac00f2ec798e02222767e3b9066f8
William Norland <william@luwisoft.com>
2024-10-15 14:18:38 +0200

    inpainting prototype

--- ImageGenInterface/genimage.py
+++ ImageGenInterface/genimage.py
@@ -1,7 +1,9 @@
 import torch
 import os
 from PIL.Image import Image as Img
+from PIL import Image
 from diffusers import StableDiffusionImg2ImgPipeline
+import sys
 
 STANDARD_NEGATIVE_PROMPT = """
     doesnt have four legs,
@@ -17,93 +19,146 @@ STANDARD_NEGATIVE_PROMPT = """
     dark
 """
 
-def generate_image(
-        prompt: str,
-        image: Img, 
-        show_image: bool, 
-        save_image_path: str = "", 
-        strength: float = 0.8, 
-        num_inf: int = 50, 
-        guidance: float = 7.5, 
-        neg_prompt= STANDARD_NEGATIVE_PROMPT
-    ) -> None:
+try:
 
-    MODEL_PATH: str = "./local_models/stable-diffusion-v1-5"
-    DEVICE: str = ""
-    CUDA = torch.cuda.is_available()
-    MPS = torch.backends.mps.is_available()
+    def generate_image(
+            prompt: str,
+            image: Img, 
+            show_image: bool, 
+            save_image_path: str = "", 
+            strength: float = 0.8, 
+            num_inf: int = 50, 
+            guidance: float = 7.5, 
+            neg_prompt= STANDARD_NEGATIVE_PROMPT
+        ) -> None:
 
-    # Check if cuda is available
-    if CUDA:
+        MODEL_PATH: str = "./local_models/stable-diffusion-v1-5"
+        DEVICE: str = ""
+        CUDA = torch.cuda.is_available()
+        MPS = torch.backends.mps.is_available()
 
-        print("CUDA is available")
+        # Check if cuda is available
+        if CUDA:
 
-        DEVICE = "cuda"
-        
-    # Check if MPS is available
-    elif MPS:
+            print("CUDA is available")
+
+            DEVICE = "cuda"
             
-        print("MPS is available")
+        # Check if MPS is available
+        elif MPS:
+                
+            print("MPS is available")
 
-        DEVICE = "mps"
+            DEVICE = "mps"
 
-        # Let MPS use all available memory, may cause system failure
-        os.environ["PYTORCH_MPS_HIGH_WATERMARK_RATIO"] = "0.0"
+            # Let MPS use all available memory, may cause system failure
+            os.environ["PYTORCH_MPS_HIGH_WATERMARK_RATIO"] = "0.0"
 
-    # If neither CUDA or MPS is available, run on CPU
-    else:
+        # If neither CUDA or MPS is available, run on CPU
+        else:
 
-        print("CUDA and MPS is unavailable, running CPU model.")
+            print("CUDA and MPS is unavailable, running CPU model.")
 
-        num_threads = os.cpu_count()
+            num_threads = os.cpu_count()
 
-        torch.set_num_threads(num_threads)  # Adjust based on your CPU
+            torch.set_num_threads(num_threads)  # Adjust based on your CPU
 
-        DEVICE = "cpu"
+            DEVICE = "cpu"
 
-    # Run the model using a diffusers pipeline
-    pipe: StableDiffusionImg2ImgPipeline = StableDiffusionImg2ImgPipeline.from_pretrained(
-        MODEL_PATH if os.path.exists(MODEL_PATH) else "runwayml/stable-diffusion-v1-5",
-        torch_dtype = torch.float32 
+        # Run the model using a diffusers pipeline
+        # We would like to use dTypr torch.float16 but it causes exceptions
+        pipe: StableDiffusionImg2ImgPipeline = StableDiffusionImg2ImgPipeline.from_pretrained(
+            MODEL_PATH if os.path.exists(MODEL_PATH) else "runwayml/stable-diffusion-v1-5",
+            torch_dtype = torch.float32 
+        )
+
+        # Disable this subdue warning
+        pipe.tokenizer.clean_up_tokenization_spaces = False
+
+        # Save the model locally if it doesn't exist
+        if not os.path.exists(MODEL_PATH): pipe.save_pretrained(MODEL_PATH)
+
+        # Set the device for the model
+        # The diffuser docs say that i should use this AFTER enabling the sequential cpu offload
+        # But this throws an exception, so i'm doing it before which works fine.
+        # Im already seeing a substantial memory reduction with the whole setup so it's fine for now.
+        pipe.to(DEVICE)
+
+        # If using GPU or MPS, enable sequential CPU offload to reduce memory usage
+        if CUDA or MPS: 
+            pipe.enable_sequential_cpu_offload(device=DEVICE)
+            
+        # Memory saving measures
+        pipe.enable_vae_tiling()
+        pipe.enable_attention_slicing()
+
+        # Prepare image, no clue if resizing and converting to RGB is necessary but it's in the example code on the diffuser docs
+        init_image = image.convert("RGB")
+        init_image = init_image.resize((768, 512))
+
+        # Generate image
+        generated_image = pipe(prompt=prompt, image=image, strength=strength, num_inference_steps=num_inf, guidance_scale=guidance, negative_prompt=neg_prompt).images[0]
+
+        print("model has finished running.")
+
+        # Display the image
+        if show_image:
+            generated_image.show()
+            print("image shown.")
+
+        # Save the image
+        if save_image_path != "":
+            generated_image.save(save_image_path)
+            print("image saved.")
+
+except Exception as e:
+    
+    print(f"Failed to setup diffusers pipeline, script failed with error message: {e}")
+    
+    sys.exit()
+
+cli_args = sys.argv
+
+try:
+
+    prompt = cli_args[1]
+
+    image = Image.open(cli_args[2])
+
+    show_image = True if cli_args[3] == "true" else False
+
+    save_image_path = cli_args[4] if cli_args[4] != "false" else ""
+
+    strength = float(cli_args[5]) 
+
+    num_inf = int(cli_args[6])
+
+    guidance = float(cli_args[7])  
+
+    neg_prompt = cli_args[8] if cli_args[8] != "false" else STANDARD_NEGATIVE_PROMPT
+
+    generate_image(
+        prompt,
+        image,
+        show_image,
+        save_image_path,
+        strength,
+        num_inf,
+        guidance,
+        neg_prompt
     )
 
-    # Disable this subdue warning
-    pipe.tokenizer.clean_up_tokenization_spaces = False
+except Exception as e:
 
-    # Save the model locally if it doesn't exist
-    if not os.path.exists(MODEL_PATH): pipe.save_pretrained(MODEL_PATH)
+    print(f"Arguments are not valid, script failed with error message: {e} \n")
 
-    # Set the device for the model
-    # The diffuser docs say that i should use this AFTER enabling the sequential cpu offload
-    # But this throws an exception, so i'm doing it before which works fine.
-    # Im already seeing a substantial memory reduction with the whole setup so it's fine for now.
-    pipe.to(DEVICE)
+    print("Correct usage: python / python3 genimage.py <prompt> <image_path> <show_image> <save_image_path> <strength> <num_inf> <guidance> <neg_prompt> \n")
 
-    # If using GPU or MPS, enable sequential CPU offload to reduce memory usage
-    if CUDA or MPS: 
-        pipe.enable_sequential_cpu_offload(device=DEVICE)
-        
-    # Memory saving measures
-    pipe.enable_vae_tiling()
-    pipe.enable_attention_slicing()
+    print("Standard inputs: for strength is 0.8, num_inf is 50, guidance is 7.5 \n")
 
-    # Prepare image, no clue if resizing and converting to RGB is necessary but it's in the example code on the diffuser docs
-    init_image = image.convert("RGB")
-    init_image = init_image.resize((768, 512))
+    print("Optional inputs: neg_prompt, show_image and save_image_path are optional, if you dont want to use them, just write 'false' in their place.")
 
-    # Generate image
-    generated_image = pipe(prompt=prompt, image=image, strength=strength, num_inference_steps=num_inf, guidance_scale=guidance, negative_prompt=neg_prompt).images[0]
+    sys.exit()
 
-    print("model has finished running.")
-
-    # Display the image
-    if show_image:
-        generated_image.show()
-        print("image shown.")
-
-    # Save the image
-    if save_image_path != "":
-        generated_image.save(save_image_path)
-        print("image saved.")
-
-    return generated_image
+    # Example usage:
+    #python3 genimage.py "Table in living room, modern, European, cozy" "Trials/original-table.jpg" true "Trials/output-11.png" 0.3 200 15 false
