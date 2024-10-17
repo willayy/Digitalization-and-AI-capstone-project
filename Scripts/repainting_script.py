c7d3b2c791bc6ec3233e7b2b247524330c40c607
William Norland <william@luwisoft.com>
2024-10-17 15:59:27 +0200

    New CLI based scripts

--- Scripts/repainting_script.py
+++ Scripts/repainting_script.py
@@ -1,130 +1,173 @@
-import torch
-import os
-from PIL.Image import Image as Img
+import torch, os, sys, argparse
+from diffusers import StableDiffusionXLImg2ImgPipeline
+from diffusers.utils import load_image
 from PIL import Image
-from diffusers import StableDiffusionImg2ImgPipeline
-import sys
+from PIL.Image import Image as Image_obj
+from argparse import ArgumentParser
 
-STANDARD_NEGATIVE_PROMPT = """
-    doesnt have four legs,
-    watermarked,
-    white background,
-    cartoonish,
-    low quality,
-    pixelated,
-    blurry,
-    distorted,
-    not realistic,
-    not detailed,
-    dark
-"""
+def hardware_accelerate_if_available() -> str:
+    DEVICE: str = ""
+    CUDA = torch.cuda.is_available()
+    MPS = torch.backends.mps.is_available()
 
-try:
+    # Check if cuda is available
+    if CUDA:
 
-    def generate_image(
-            prompt: str,
-            image: Img, 
-            show_image: bool, 
-            save_image_path: str = "", 
-            strength: float = 0.8, 
-            num_inf: int = 50, 
-            guidance: float = 7.5, 
-            neg_prompt= STANDARD_NEGATIVE_PROMPT
-        ) -> None:
+        DEVICE = "cuda"
+        
+    # Check if MPS is available
+    elif MPS:
 
-        MODEL_PATH: str = "./local_models/stable-diffusion-v1-5"
+        DEVICE = "mps"
 
-        # Run the model using a diffusers pipeline
-        # We would like to use dTypr torch.float16 but it causes exceptions
-        pipe: StableDiffusionImg2ImgPipeline = StableDiffusionImg2ImgPipeline.from_pretrained(
-            MODEL_PATH if os.path.exists(MODEL_PATH) else "runwayml/stable-diffusion-v1-5",
-            torch_dtype = torch.float32 
-        )
+        # Let MPS use all available memory, may cause system failure
+        os.environ["PYTORCH_MPS_HIGH_WATERMARK_RATIO"] = "0.0"
 
-        # Save the model locally if it doesn't exist
-        if not os.path.exists(MODEL_PATH): pipe.save_pretrained(MODEL_PATH)
+    # If neither CUDA or MPS is available, run on CPU
+    else:
 
-        # Set the device for the model
-        # The diffuser docs say that i should use this AFTER enabling the sequential cpu offload
-        # But this throws an exception, so i'm doing it before which works fine.
-        # Im already seeing a substantial memory reduction with the whole setup so it's fine for now.
-        pipe.to(DEVICE)
+        num_threads = os.cpu_count()
 
-        # If using GPU or MPS, enable sequential CPU offload to reduce memory usage
-        if CUDA or MPS: 
-            pipe.enable_sequential_cpu_offload(device=DEVICE)
-            
-        # Memory saving measures
-        pipe.enable_vae_tiling()
-        pipe.enable_attention_slicing()
+        torch.set_num_threads(num_threads-1)  # Adjust based on your CPU
 
-        # Prepare image, no clue if resizing and converting to RGB is necessary but it's in the example code on the diffuser docs
-        init_image = image.convert("RGB")
-        init_image = init_image.resize((768, 512))
+        DEVICE = "cpu"
 
-        # Generate image
-        generated_image = pipe(prompt=prompt, image=image, strength=strength, num_inference_steps=num_inf, guidance_scale=guidance, negative_prompt=neg_prompt).images[0]
+    return DEVICE
 
-        print("model has finished running.")
+def enable_memory_save_mode(pipeline: StableDiffusionXLImg2ImgPipeline) -> None:
+    CUDA_AVAILABLE = torch.cuda.is_available()
+    MPS_AVAILABLE = torch.backends.mps.is_available()
+    # If using GPU or MPS, enable sequential CPU offload to reduce memory usage
+    if CUDA_AVAILABLE or MPS_AVAILABLE: pipeline.enable_sequential_cpu_offload(device=DEVICE)
+    # Memory saving measures
+    pipeline.enable_vae_tiling()
+    pipeline.enable_attention_slicing()
 
-        # Display the image
-        if show_image:
-            generated_image.show()
-            print("image shown.")
+def init_arg_parser() -> ArgumentParser:
+    """
+    Initializes an ArgumentParser for an AI image generation / inpainting script
+    using the runwayml/stable-diffusion-inpainting and runwayml/stable-diffusion-v1-5
+    """
 
-        # Save the image
-        if save_image_path != "":
-            generated_image.save(save_image_path)
-            print("image saved.")
+    STANDARD_NEGATIVE_PROMPT = "watermarked, cartoonish, low quality, pixelated, blurry, distorted, un-realistic, dark"
 
-except Exception as e:
-    
-    print(f"Failed to setup diffusers pipeline, script failed with error message: {e}")
-    
-    sys.exit()
+    parser = argparse.ArgumentParser(description="An image inpainting script based on the diffusers module")
 
-cli_args = sys.argv
-
-try:
-
-    prompt = cli_args[1]
-
-    image = Image.open(cli_args[2])
-
-    show_image = True if cli_args[3] == "true" else False
-
-    save_image_path = cli_args[4] if cli_args[4] != "false" else ""
-
-    strength = float(cli_args[5]) 
-
-    num_inf = int(cli_args[6])
-
-    guidance = float(cli_args[7])  
-
-    neg_prompt = cli_args[8] if cli_args[8] != "false" else STANDARD_NEGATIVE_PROMPT
-
-    generate_image(
-        prompt,
-        image,
-        show_image,
-        save_image_path,
-        strength,
-        num_inf,
-        guidance,
-        neg_prompt
+    parser.add_argument(
+        "-p",
+        required=False,
+        default=False,
+        action="store_true",
+        help="Enable performance mode (WARNING: Takes up a lot of memory)"
     )
 
-except Exception as e:
+    parser.add_argument(
+        "-show",
+        required=False,
+        default=False,
+        action="store_true",
+        help="(Optional) Show image when its generated."
+    )
 
-    print(f"Arguments are not valid, script failed with error message: {e} \n")
+    parser.add_argument(
+        "--init_image",
+        type=str,
+        required=True,
+        help="The path to the initial image given to the AI model."
+    )
 
-    print("Correct usage: python / python3 genimage.py <prompt> <image_path> <show_image> <save_image_path> <strength> <num_inf> <guidance> <neg_prompt> \n")
+    parser.add_argument(
+        "--prompt",
+        type=str,
+        required=True,
+        help="The prompt for the image inpainting"
+    )
 
-    print("Standard inputs: for strength is 0.8, num_inf is 50, guidance is 7.5 \n")
+    parser.add_argument(
+        "--n_prompt",
+        type=str,
+        required=False,
+        default=STANDARD_NEGATIVE_PROMPT,
+        help="(Optional) The negative prompt for the image inpainting, this tells the model what to avoid"
+    )
 
-    print("Optional inputs: neg_prompt, show_image and save_image_path are optional, if you dont want to use them, just write 'false' in their place.")
+    parser.add_argument(
+        "--strength",
+        type=float,
+        required=False,
+        default=1, 
+        help="(Optional) The strength of the image inpainting, this controls how much creative liberty the model gets."
+    )
 
-    sys.exit()
+    parser.add_argument(
+        "--num_inf",
+        type=int,
+        required=False,
+        default=50,
+        help="(Optional) The number of inference cycles performed during inpainting, higher inference takes longer times but usually give more well defined pictures."
+    )
 
-    # Example usage:
-    #python3 genimage.py "Table in living room, modern, European, cozy" "Trials/original-table.jpg" true "Trials/output-11.png" 0.3 200 15 false
+    parser.add_argument(
+        "--guidance",
+        type=float,
+        required=False,
+        default=7.5,
+        help="(Optional) The models guidance value, controls how much the model listens to the input prompt."
+    )
+
+    return parser
+
+parser = init_arg_parser()
+
+args = parser.parse_args()
+
+# Constants for this model
+MODEL_NAME: str = "stabilityai/stable-diffusion-xl-refiner-1.0" #"runwayml/stable-diffusion-inpainting" 
+MODEL_PATH: str = f"./local_models/{MODEL_NAME}" # Path of local model
+
+# Parse args
+GUIDANCE: float = args.guidance
+NEGATIVE_PROMPT: str = args.n_prompt
+NUM_INF: int = args.num_inf
+OBJ_IMAGE_PATH: str = args.init_image
+PROMPT: str = args.prompt
+SHOW: bool = args.show
+STRENGTH: float = args.strength
+VANILLA: bool = not args.p
+
+# Get device
+DEVICE: str = hardware_accelerate_if_available()
+
+pipeline = StableDiffusionXLImg2ImgPipeline.from_pretrained(
+    MODEL_PATH if os.path.exists(MODEL_PATH) else MODEL_NAME,
+    torch_dtype=torch.float32
+)
+
+# Save model if it isnt saved yet
+if not os.path.exists(MODEL_PATH): pipeline.save_pretrained(MODEL_PATH)
+
+# Tell the model to pipe to the right device
+pipeline.to(DEVICE)
+
+# Conditionally enable memory saving measures
+if VANILLA: enable_memory_save_mode(pipeline)
+
+# Initialize images from a path
+init_image = load_image(Image.open(OBJ_IMAGE_PATH)).resize((512,512))
+
+image: Image_obj = pipeline(
+    prompt=PROMPT,
+    negative_prompt=NEGATIVE_PROMPT,
+    image=init_image,
+    strength=STRENGTH,
+    num_inference_steps=NUM_INF,
+    guidance_scale=GUIDANCE
+
+).images[0]
+
+if SHOW: image.show()
+
+sys.exit(0)
+
+# EXAMPLE USE:
+# python3 repainting_script.py -show --init_image Trials/original-image-small.jpg --prompt "Place this table in an european livingroom, the tables leg should be on the floor" --n_prompt "Changing table shape, changing table color"
