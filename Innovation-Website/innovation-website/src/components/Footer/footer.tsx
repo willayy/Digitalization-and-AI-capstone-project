f7d5d85a74c52e0139ee12e0b511d91dde78f657
Mandus <mandus.hogberg@gmail.com>
2024-10-20 17:32:02 +0200

    footer changed

--- Innovation-Website/innovation-website/src/components/Footer/footer.tsx
+++ Innovation-Website/innovation-website/src/components/Footer/footer.tsx
@@ -23,22 +23,37 @@ export default function Footer() {
         <Link href="/">SKAPA</Link>
       </h1>
 
-      <nav className={styles.nav}>
-        <ul style={{ display: 'flex', gap: 0 }}>
-          <li>
-            <Link href="/references" className={styles.navItem}>References</Link>
-          </li>
-          <li>
-            <Link href="/design" className={styles.navItem}>Design</Link>
-          </li>
-          <li>
+      
+
+      <div className={styles.containerdiv}>
+        <div className={styles.smalldiv}>
+          <h3 className={styles.footerh3}>Homepage</h3> 
+            <Link href="/" className={styles.navItem}>Home</Link>
+        </div>
+
+        <div className={styles.smalldiv}>
+          <h3 className={styles.footerh3}>Made by</h3>
+            <Link href="/about" className={styles.navItem}>Team 12</Link>
+        </div>
+
+        <div className={styles.smalldiv}>
+          <h3 className={styles.footerh3}>Why this product</h3> 
             <Link href="/study" className={styles.navItem}>Study</Link>
-          </li>
-          <li>
-            <Link href="/about" className={styles.navItem}>About us</Link>
-          </li>
-        </ul>
-      </nav>
+        </div>
+
+        <div className={styles.smalldiv}>
+          <h3 className={styles.footerh3}>Our sources</h3> 
+            <Link href="/references" className={styles.navItem}>References</Link>
+        </div>
+
+        <div className={styles.smalldiv}>
+          <h3 className={styles.footerh3}>More on the product</h3> 
+            <Link href="/design" className={styles.navItem}>Design</Link>
+        </div>
+
+
+
+      </div>
     </footer>
   );
 }
