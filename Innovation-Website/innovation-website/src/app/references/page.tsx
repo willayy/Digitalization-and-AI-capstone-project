71c20b801e88ae12b6e53dbca735ff198dd14aa2
Mandus <mandus.hogberg@gmail.com>
2024-10-26 20:09:50 +0200

    made footer on references work

--- Innovation-Website/innovation-website/src/app/references/page.tsx
+++ Innovation-Website/innovation-website/src/app/references/page.tsx
@@ -5,7 +5,7 @@ export default function References() {
   return (
     <div>
         <div className="page">
-          <div className="section">
+          <div className="section" style={{height: '75vh'}}>
             <h1 className={styles.text}>References</h1>
             <ol className={styles.list}>
               <li className={styles.text}>Hage, S., Francl, T., Bardou, E., & Söderqvist, J. (2024). <i>IKEA X Chalmers Kickoff</i> [PowerPoint Slides]. Digitalization and AI in practice, Chalmers Tekniska Högskola. September 5.</li>
